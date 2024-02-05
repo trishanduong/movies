06663fde81e27ae3ebea8e3900040d68ed510b0a
trisha <trishanduong@gmail.com>
2024-02-04 21:46:20 -0800

    Review REST Controller functionality (POST/GET Requests) - Tested on Postman

---
+++ src/main/java/dev/trisha/movies/ReviewService.java
@@ -0,0 +1,28 @@
+package dev.trisha.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review.getId()))
+                .first();
+        return review;
+    }
+}
+
+/* We need a template to talk to the database. This is an alternative to using a repository (dynamic query)  */
